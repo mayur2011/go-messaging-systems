45e6e855819d8e661ce7bb4cbac22563dfe6cec8
mayur2011 <er.mayur2011@gmail.com>
2023-08-15 17:02:53 +0530

    NATS nc.Request msg.Respond impl

--- nats/basic/1_req_resp/subscriber01/main.go
+++ nats/basic/1_req_resp/subscriber01/main.go
@@ -8,10 +8,31 @@ import (
 )
 
 func main(){
+	fmt.Println("Starting NATS Server...")
 	url:= "nats://localhost:4222"
-	nc, _:= nats.Connect(url)
+	nc, err:= nats.Connect(url)
+	if err  != nil{
+		logrus.Fatal("Error connecting NATS Server...")
+	}
 	defer nc.Close()
 
-	// Su
+	count:= 0
+	sub, _ := nc.Subscribe("events.local", func(msg *nats.Msg){
+		count++
+		fmt.Printf("Msg received on sub: %v, Data: %v\n", msg.Subject, string(msg.Data))
+		if msg.Reply != "" {
+			msg.Respond([]byte("OK"))
+		}
+	})
+	defer sub.Unsubscribe()
 
+	for {
+		old:= count
+		time.Sleep(15*time.Second)
+		if old == count {
+			fmt.Println("No msg to consume..!")
+			break
+		}
+	}
+	fmt.Println("Processed %v messages\n", count)
 }
